f52c4878ec307070c24350a5f7ad4ba369ef69d1
busrabahar <busra.bahar89@gmail.com>
2021-07-29 16:07:27 +0300

    update

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ApplicationRecord
 	has_many :enrollments
 	has_many :courses, through: :enrollments
-	validates :name, prensence: true
+	validates :name, presence: true
 	validates :name, uniqueness: true
 end
